803a8e49ef4c47396b01ada92fee7955b6efa3a5
quanghatran <quanghatran2161998@gmail.com>
2024-02-26 18:08:11 +0700

    backend: update user info

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -7,6 +7,9 @@ import {
   registrationUser,
   socialAuth,
   updateAccessToken,
+  updateAvatar,
+  updatePassword,
+  updateUserInfo,
 } from "../controllers/user.controller";
 import { isAuthenticated } from "./../middleware/auth";
 
@@ -19,5 +22,8 @@ userRouter.get("/logout", isAuthenticated, logoutUser);
 userRouter.get("/refresh-token", updateAccessToken);
 userRouter.get("/me", isAuthenticated, getUserInfo);
 userRouter.post("/social-auth", socialAuth);
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
+userRouter.put("/update-user-password", isAuthenticated, updatePassword);
+userRouter.put("/update-user-avatar", isAuthenticated, updateAvatar);
 
 export default userRouter;
--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -1,12 +1,16 @@
 import { Response } from "express";
-import userModel from "../models/user.model";
+import { redis } from "../utils/redis";
 
 // get user by id
 export const getUserById = async (id: string, res: Response) => {
-  const user = await userModel.findById(id);
+  const userJson = await redis.get(id);
 
-  res.status(201).json({
-    success: true,
-    user: user,
-  });
+  if (userJson) {
+    const user = JSON.parse(userJson);
+
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  }
 };
