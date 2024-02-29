7520c2bf089a6c1b5cdf285e1698160a4732d6d2
quanghatran <quanghatran2161998@gmail.com>
2024-02-29 15:50:38 +0700

    backend: add & edit course

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import { ErrorMiddleware } from "./middleware/error";
+import courseRouter from "./routes/course.route";
 import userRouter from "./routes/user.route";
 export const app = express();
 
@@ -21,6 +22,7 @@ app.use(
 
 // routes
 app.use("/api/v1", userRouter);
+app.use("/api/v1", courseRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
